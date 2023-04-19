91ae9ed22708ef64d7fb35743188b001ea772629
Klaus <BitLCEntwicklerDortmund@web.de>
2023-04-19 14:37:03 +0200

    Add project files.

---
+++ PiratesCove_rudimentaer/Bus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiratesCove_rudimentaer {
+    internal class Bus {
+        private Queue<Golfer> q = new Queue<Golfer>();
+
+        public void addGolfer(Golfer g) {
+            q.Enqueue(g);
+        }
+
+        public void zeigeGolfer() {
+            foreach (Golfer g in q) {
+                Console.WriteLine(g.Name);
+            }
+        }
+
+        public void zeigeGolferRueckwaerts() {
+            // alternativ Reverse-Methode einsetzen
+            List<Golfer> lG = q.ToList();
+
+            for (int i = lG.Count - 1; i >= 0; i--) {
+                Console.WriteLine(lG[i].Name);
+            }
+        }
+
+    }
+}
---
+++ PiratesCove_rudimentaer/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace PiratesCove_rudimentaer {
+    internal class Program {
+        static void Main(string[] args) {
+            //Console.WriteLine("Hallo, Golfer!");
+
+            //Golfer g1 = new Golfer() { Name="Toni"};
+            //Golfer g2 = new Golfer() { Name = "Franz" };
+
+            //Queue<Golfer> q = new Queue<Golfer>();
+            //q.Enqueue(g1);
+            //q.Enqueue(g2);
+
+            ////Console.WriteLine( q.Peek().Name );
+            ////q.Dequeue();
+            ////Console.WriteLine(q.Peek().Name);
+
+            //List<Golfer> lG = q.ToList();            
+
+            //for (int i = lG.Count-1; i >= 0; i--) {
+            //    Console.WriteLine(lG[i].Name);
+            //    //Console.WriteLine(i);
+            //}
+            Bus bus = new Bus();
+            bus.addGolfer(new Golfer() { Name="Toni"});
+            bus.addGolfer(new Golfer() { Name="Franz"});
+
+            bus.zeigeGolfer();
+            bus.zeigeGolferRueckwaerts();
+
+            Console.Read();
+        }
+    }
+
+    internal class Golfer {
+        public string Name { get; set; }
+    }
+}
